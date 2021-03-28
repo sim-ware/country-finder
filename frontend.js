897bff22fa47b1a5356c5173e903f4056a879f74
sim-ware <samir.gossain@gmail.com>
2021-03-28 22:21:12 +0100

    adds dog example to map a json object

--- frontend.js
+++ frontend.js
@@ -1,4 +1,16 @@
 function myFunction() {
   console.log('Input Value', document.getElementById("country").value)
   // 'https://dev.to/adebayoileri/configure-babel-for-nodejs-application-3798'
+
+  const dogs = [
+    { name: 'Snickers', age: 2 },
+    { name: 'Hugo', age: 8 },
+    { name: 'Sunny', age: 1 }
+  ];
+  const markup = `
+  <ul class="dogs">
+      ${dogs.map(dog => `<li>* ${dog.name} is ${dog.age * 7}</li>`)}
+  `;
+
+  document.getElementById("result").innerHTML = markup
 }
